4ea33acfd9c3becd945af453f75d32b23ee14f96
dbjdbj <dbj@dbj.org>
2020-10-18 21:45:51 +0200

    reshuffle so it is easier to read

--- units/testu.c
+++ units/testu.c
@@ -4,6 +4,15 @@
 
 #include "../ubench.h/ubench.h"
 
+#ifndef _MSC_VER
+#error This is Windows build only
+#endif
+
+//
+// the way of 'fixtures' but without macros
+// so that one can see what is happening 
+// on windows build
+//
 
 // https://codingforspeed.com/c-coding-example-using-hash-algorithm-to-remove-duplicate-number-by-on/
 
@@ -23,7 +32,7 @@ static int hashKey(long x) {
     return x;
 }
 #else // _WIN64
-uint64_t hashKey(uint64_t x) {
+static uint64_t hashKey(uint64_t x) {
     x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
     x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
     x = x ^ (x >> 31);
@@ -31,31 +40,25 @@ uint64_t hashKey(uint64_t x) {
 }
 #endif // _WIN64
 /******************************************************************/
-/** remove duplicates from an array                              **/
+// this is the 'fixture'
+struct testu_dbj_data {
+int TESTU_HASH /* = TESTU_HASHSIZE - 1 */;
+int hasharr[TESTU_HASHSIZE];
+long data[TESTU_MAXN] /* = { 0L }*/ ;
+};
 /******************************************************************/
-UBENCH( dbj, arr_of_uniques ) {
 
-static long data[TESTU_MAXN] = { 0L } ;
-
-// make random data
-srand (time(NULL));
-for (int i = 0; i < TESTU_MAXN; i ++) data[i] = rand() % TESTU_MAXN;
-
-// we init the hash table by using the special value
-for (int i = 0; i < TESTU_HASHSIZE; i ++) hasharr[i] = FREE_SLOT_MARK;
-
-// And, the following O(n) algorithm will try each number and check if 
-// it appears in the table first (almost O(1) lookup).
+static void * produce_uniques ( struct testu_dbj_data * ubench_fixture)  {
 int count = 0;
 for (int i = 0; i < TESTU_MAXN; i ++) {
-        int key = hashKey(data[i]); // unbound hash key
+        int key = hashKey(ubench_fixture->data[i]); // unbound hash key
         key &= TESTU_HASH;  // bound it to hash index
         for (;;) {
-                if (hasharr[key] == FREE_SLOT_MARK) { // slot is available
-                    hasharr[key] = i;  // means the number appears first time
+                if (ubench_fixture->hasharr[key] == FREE_SLOT_MARK) { // slot is available
+                    ubench_fixture->hasharr[key] = i;  // means the number appears first time
                     break;
                 }
-                else if (data[hasharr[key]] == data[i]) { // same number
+                else if (ubench_fixture->data[hasharr[key]] == ubench_fixture->data[i]) { // same number
                     count ++;
                     break;
                 } else {
@@ -64,27 +67,8 @@ for (int i = 0; i < TESTU_MAXN; i ++) {
         }
     }
 
-    // uniques are in hasharr
-#if 0
-// The next piece of code loops the hash table and print out the unique numbers.
-printf("\n %s -- %d numbers removed.\n", __FUNCSIG__, count );
-
- printf("\n{ ");
-    for (int i = 0; i < TESTU_HASHSIZE; i ++) {
-        if (hasharr[i] != TESTU_HASHSIZE) {
-            printf("%d ", data[hasharr[i]] );
-        }
-    }
- printf("}\n");
-#endif // 0
+    return ubench_fixture;
 }
-/******************************************************************/
-// enum { TESTU_HASHSIZE = 1048576 };  // 20MB
-struct testu_dbj_data {
-int TESTU_HASH /* = TESTU_HASHSIZE - 1 */;
-int hasharr[TESTU_HASHSIZE];
-long data[TESTU_MAXN] /* = { 0L }*/ ;
-};
 
 #if 0
 UBENCH_F_SETUP(testu_dbj_data) 
@@ -120,28 +104,6 @@ static void ubench_f_teardown_testu_dbj_data
  memset(ubench_fixture->hasharr, 0, __crt_countof(ubench_fixture->hasharr) ) ;
 }
 
-static void * produce_uniques ( struct testu_dbj_data * ubench_fixture)  {
-int count = 0;
-for (int i = 0; i < TESTU_MAXN; i ++) {
-        int key = hashKey(ubench_fixture->data[i]); // unbound hash key
-        key &= TESTU_HASH;  // bound it to hash index
-        for (;;) {
-                if (ubench_fixture->hasharr[key] == FREE_SLOT_MARK) { // slot is available
-                    ubench_fixture->hasharr[key] = i;  // means the number appears first time
-                    break;
-                }
-                else if (ubench_fixture->data[hasharr[key]] == ubench_fixture->data[i]) { // same number
-                    count ++;
-                    break;
-                } else {
-                    key = (key + 1) & TESTU_HASH; // different number, collision happens
-                }
-        }
-    }
-
-    return ubench_fixture;
-}
-
 #if 0
 UBENCH_F(testu_dbj_data, dbj_uniques_arr ) 
 {
@@ -168,11 +130,14 @@ static void ubench_f_testu_dbj_data_dbj_uniques_arr(ubench_int64_t *const ns, co
         } 
         ubench_f_teardown_testu_dbj_data(&fixture); 
    } 
+   
    static void __cdecl ubench_register_testu_dbj_data_dbj_uniques_arr(void); 
+
    __pragma(comment(linker, "/include:" "ubench_register_testu_dbj_data_dbj_uniques_arr" "_")); 
    __declspec(allocate(".CRT$XCU")) 
      void(__cdecl * ubench_register_testu_dbj_data_dbj_uniques_arr_)(void) 
          = ubench_register_testu_dbj_data_dbj_uniques_arr; 
+   
    static void __cdecl ubench_register_testu_dbj_data_dbj_uniques_arr(void)
     { 
         const size_t index = ubench_state.benchmarks_length++; 
