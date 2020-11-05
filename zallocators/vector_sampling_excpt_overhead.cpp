93045da1531b52ac5d921597c8179003de2e51cc
dbj <dbj@dbj.org>
2020-11-05 22:04:09 +0100

    update

--- zallocators/vector_sampling_excpt_overhead.cpp
+++ zallocators/vector_sampling_excpt_overhead.cpp
@@ -2,13 +2,10 @@
 /// https://github.com/attractivechaos/benchmarks
 
 #include "../ubench/ubench.h"
+#include "../dbj-fwk/printing_macros.h"
 
 #include <string>
 #include <stdexcept>
-
-// just for this file
-enum { RESET, GREEN, RED };
-static char* colours_[] = { (char*)"\033[0m", (char*)"\033[32m", (char*)"\033[31m" };
 
 #ifdef _DEBUG
 constexpr static  bool debugging_ = true ;
@@ -26,6 +23,56 @@ static const test_array_type test_array_element = std::string(__FILE__);
 #ifndef _DEBUG
 #if __has_include(<atlsimpcoll.h>)
 
+#include <atlcoll.h>
+
+using atl_arr = ATL::CAtlArray<test_array_type>;
+
+static void atl_array() 
+{
+	// _ATL_NO_EXCEPTIONS ?
+#if _HAS_EXCEPTIONS
+	try {
+#endif // _HAS_EXCEPTIONS
+		atl_arr array_;
+		// add 3 elements
+		array_.Add(test_array_element);
+		array_.Add(test_array_element);
+		array_.Add(test_array_element);
+		// use illegal index
+		(void)array_.operator[](9);
+#if _HAS_EXCEPTIONS
+	}
+	catch (...) {
+	}
+#endif // _HAS_EXCEPTIONS
+
+}
+
+UBENCH(bad_index_vector, atl_arr)
+{
+	static bool done_that = false;
+	// SEH is always available but not mixed
+	// in the same function
+	// with C++ exceptions
+	__try {
+		atl_array();
+	}
+	__except (
+		GetExceptionCode() == EXCEPTION_ARRAY_BOUNDS_EXCEEDED
+		? EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_EXECUTION)
+	{
+		// RELEASE or DEBUG build
+		// on bad index ATL raises SE with id: EXCEPTION_ARRAY_BOUNDS_EXCEEDED
+		// which is defined in minwinbase.h
+		// ATL never throws C++ exceptions
+		if (!done_that) {
+			DBJ_WARN("ATL index out of range caught by SEH, on each call!");
+			done_that = true;
+		}
+
+	}
+}
+
 #include <atlsimpcoll.h>
 using atl_vec = ATL::CSimpleArray<test_array_type>;
 
@@ -40,6 +87,27 @@ static void atl_simple_array() {
 		array_.Add(test_array_element);
 		// use illegal index
 		(void)array_.operator[](9);
+/*
+
+AtlDef.h approx line 171
+
+#ifndef AtlThrow
+#ifndef _ATL_CUSTOM_THROW
+#define AtlThrow ATL::AtlThrowImpl
+#endif
+
+ATL::AtlThrowImpl(); can be compiled to throw c++ exceotions or raise SE 
+regarding the exsitence of _ATL_NO_EXCEPTIONS
+
+The problem: ATL::CSimpleArray<T>; does not use AtlThrow
+Thus even if the host is compiled with /EHsc its instance will raise the SE
+on e.g. range error in its operator []
+Same issue is with ATL::CSimpleMap<K,V> ;
+
+My comment: this is why ATL::CSimpleArray<T> is very fast
+
+*/
+
 #if _HAS_EXCEPTIONS
 	}
 	catch ( ...) {
@@ -66,7 +134,7 @@ UBENCH(bad_index_vector, atl_simple_arr)
 	// which is defined in minwinbase.h
 	// ATL never throws C++ exceptions
 		if (!done_that) {
-			printf("%sATL index out of range caught by SEH, on each call!%s\n", colours_[RED], colours_[RESET]);
+			DBJ_WARN("ATL index out of range caught by SEH, on each call!");
 			done_that = true;
 		}
 
@@ -107,14 +175,14 @@ UBENCH(bad_index_vector, ms_stl_vec_)
 ///-----------------------------------------------
 /// EASTL2020 CORE
 
-/// if no C++ exceptions, EASTL will call debug break
+/// if no C++ exceptions AND debug build
+/// EASTL will call debug break
 /// which will attach to the debuger if one is running
-/// or will exit the app if debugger is not running
-/// thus, EASTL doe not use SEH
-/// it exists if c++ exceptions as switched of
+/// and keep you on that same spot
+/// 
+/// EASTL doe not use SEH
 
-// #if EASTL_EXCEPTIONS_ENABLED
-
+#ifndef _DEBUG
 
 #include <EASTL/vector.h>
 #include <EASTL/string.h>
@@ -159,11 +227,12 @@ static bool done_that = false;
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER) {
 		if ( !done_that ) {
-			printf("%sEASTL index out of range caught by SEH, on each call!%s\n", colours_[RED], colours_[RESET]);
+			DBJ_WARN("EASTL index out of range caught by SEH, on each call!");
 			done_that = true;
 		}
 	}
 }
 
-// #endif // _HAS_EXCEPTIONS
+#endif // _DEBUG
+
 
