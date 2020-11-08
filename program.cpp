9667c5fff4c3016bd3e8b6b23e921963be5fe69f
dbj <dbj@dbj.org>
2020-11-08 23:35:22 +0100

    update

--- program.cpp
+++ program.cpp
@@ -1,22 +1,14 @@
 
-/// default is log to file, MT resilient, no console
-/// unless you define your combination differently that is
-/// as we did here
-#define  DBJ_LOG_DEFAULT_SETUP DBJ_LOG_DEFAULT_WITH_CONSOLE
+/// the mandatory and only initialization of the dbj simplelog
+
 #include "dbj--simplelog/dbj_simple_log_host.h"
 
-struct simple_log_protector final {
+#ifdef _DEBUG
+int dbj_simple_log_setup_ = ( DBJ_LOG_MT | DBJ_LOG_TESTING ) ;
+#else // ! _DEBUG
+int dbj_simple_log_setup_ = (DBJ_LOG_DEFAULT_WITH_CONSOLE);
+#endif // _DEBUG
 
-	simple_log_protector() noexcept {
-		dbj_simple_log_startup(__argv[0] );
-	}
-
-	~simple_log_protector() noexcept {
-		_ASSERTE(dbj_log_finalize() == EXIT_SUCCESS);
-	}
-};
-
-static const simple_log_protector protector_;
 
 // we can NOT mix utest and ubench 
 #ifdef DBJ_USE_UBENCH
