a864717bba72c675a32058a2951fe866cb20df41
dbj <dbj@dbj.org>
2020-11-13 16:04:28 +0100

    update

--- program.cpp
+++ program.cpp
@@ -1,38 +1,24 @@
-/// ------------------------------------------------------------------------
-/// obviously this whole file will goes into the DBJ+FWK too
-/// 
 
 /// ------------------------------------------------------------------------
 /// this goes to DBJ+FWK main.cpp
 /// since it knows is it on gui or console windows app
 /// 
+#include "dbj-fwk/meta.h"
+#include "dbj-fwk/win/win_cli_args.h"
+
 /// the mandatory and only initialization of the dbj simplelog
 #include "dbj--simplelog/dbj_simple_log_host.h"
 #ifdef _DEBUG
-int dbj_simple_log_setup_ = ( DBJ_LOG_MT /*| DBJ_LOG_TESTING*/ ) ;
+int dbj_simple_log_setup_ = ( DBJ_LOG_MT ) ;
 #else // ! _DEBUG
 int dbj_simple_log_setup_ = (DBJ_LOG_DEFAULT_WITH_CONSOLE);
 #endif // _DEBUG
 
-
-// we can NOT mix utest and ubench 
-//#ifdef DBJ_USE_UBENCH
-//#ifdef DBJ_USE_UTEST
-//#error Currently can not accomodate UBENCH and UTEST in the same program
-//#endif 
-//#endif 
-
-// we will conditionaly compile for one or the other
-
-#ifdef DBJ_USE_UBENCH
 #include "ubut/ubench.h"
 UBENCH_STATE();
-#endif
 
-#ifdef DBJ_USE_UTEST
 #include "ubut/utest.h"
 UTEST_STATE();
-#endif
 
 bool are_we_on_required_os(void);
 
@@ -41,26 +27,27 @@ bool are_we_on_required_os(void);
 // framework is where SEH is managed
 extern "C" int program(int argc, char** argv)
 {
-#ifdef DBJ_USE_UBENCH
+	if(!app_args_callback_(DBJ_CL_ARG_IGNORE_UBENCH, nullptr ))
+	{
+		UBUT_INFO(" ");
+		UBUT_INFO("================================================================");
+		UBUT_INFO("UBENCHES");
+		UBUT_INFO("================================================================");
+		UBUT_INFO(" ");
 
-	UBUT_INFO(" ");
-	UBUT_INFO("================================================================");
-	UBUT_INFO("UBENCHES");
-	UBUT_INFO("================================================================");
-	UBUT_INFO(" ");
+		(void)ubench_main(argc, argv);
+	}
 
-	(void)ubench_main(argc, argv);
-#endif
+	if (!app_args_callback_(DBJ_CL_ARG_IGNORE_UTEST, nullptr))
+	{
+		UBUT_INFO(" ");
+		UBUT_INFO("================================================================");
+		UBUT_INFO("UTESTS");
+		UBUT_INFO("================================================================");
+		UBUT_INFO(" ");
 
-#ifdef DBJ_USE_UTEST
-	UBUT_INFO(" ");
-	UBUT_INFO("================================================================");
-	UBUT_INFO("UTESTS");
-	UBUT_INFO("================================================================");
-	UBUT_INFO(" ");
-
-	(void)utest_main(argc, argv);
-#endif
+		(void)utest_main(argc, argv);
+	}
 
 	UBUT_INFO(" ");
 	UBUT_INFO("================================================================");
